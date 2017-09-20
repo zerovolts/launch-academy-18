baa13710cee5cad59cf9d324996de91a0718b352
Zach Stone <hexaphixylix@yahoo.com>
2017-09-20 18:10:21 -0400

    modify launch-academy-faq-part-1 to only have one question selected

--- launch-academy-faq-part-1/src/components/App.js
+++ launch-academy-faq-part-1/src/components/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import Question from './Question'
 
-const App = props =>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedId: null,
+      questions: props.data
+    }
+  }
+
+  selectQuestion(id) {
+    if (id == this.state.selectedId) {
+      this.setState({selectedId: null})
+    } else {
+      this.setState({selectedId: id})
+    }
+  }
+
+  render() {
+    let questions = this.state.questions.map(question =>
+      <Question
+        hidden={question.id == this.state.selectedId ? false : true}
+        question={question.question}
+        answer={question.answer}
+        key={question.id}
+        selectThis={() => this.selectQuestion(question.id)} />
+    )
+
+    return (
+      <div>
+        <h1>We're here to help</h1>
+        {questions}
+      </div>
+    )
+  }
+}
+
+/*const App = props =>
   <div>
     <h1>We're here to help</h1>
     {props.data.map(question =>
       <Question question={question.question} answer={question.answer} key={question.id} />
     )}
-  </div>
+  </div>*/
 
 export default App;
--- launch-academy-faq-part-1/src/components/Question.js
+++ launch-academy-faq-part-1/src/components/Question.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
-class Question extends React.Component {
+const Question = props => {
+  let icon = props.hidden ?
+    <i className="fa fa-plus-square fa-lg hidden" aria-hidden="true"></i> :
+    <i className="fa fa-minus-square fa-lg visible" aria-hidden="true"></i>
+
+  return(
+    <div className="question-block" onClick={props.selectThis}>
+      <div>
+        {icon}<span className="question">{props.question}</span>
+      </div>
+      <div className="answer" hidden={props.hidden}>{props.answer}</div>
+      <hr />
+    </div>
+  )
+}
+
+/*class Question extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -31,6 +47,6 @@ class Question extends React.Component {
       </div>
     )
   }
-}
+}*/
 
 export default Question;
